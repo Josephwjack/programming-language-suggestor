2e8416fcb0a89ca0c8d1f79a182903e6cfd1e0cb
Josephwjack <joseph.w.jack@gmail.com>
2022-06-03 12:27:38 -0700

    add functions and values to variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,37 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
 $(document).ready(function() {
-  $("form#results").submit(function(event) {
+  $("form#info").submit(function(event) {
     event.preventDefault();
-    let name1 = $("#name1").val();
+    const name1Input = $("input#name1").val();
+    const surveyDateInput = $("#surveyDate").val();
+    const notice = $("input:radio[name=notice]:checked").val();
+    const finerThings = $("input:radio[name=finerThings]:checked").val();
+    const experience = $("input:radio[name=experience]:checked").val();
+    const car = $("input:radio[name=car]:checked").val();
+    const obstacles = $("input:radio[name=obstacles]:checked").val();
 
+
+
+    $(".name1").text(name1Input);
+    $(".surveyDate").text(surveyDateInput);
+    $(".radio1").text(notice);
+    $(".radio2").text(finerThings);
+    $(".radio3").text(experience);
+    $(".radio4").text(car);
+    $(".radio5").text(obstacles);
+    
+
+    $(".receipt").show();
+    $("form#info").hide();
   });
-});     
+});
+
+
+
+
+
+
+
+
+
+
+
