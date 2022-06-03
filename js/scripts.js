4b8d854548ac232340c547f88707233c94984a9f
Josephwjack <joseph.w.jack@gmail.com>
2022-06-03 13:48:55 -0700

    add result show and hide options

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,29 @@ $(document).ready(function() {
 
     if (name1Input === undefined || surveyDateInput === undefined || notice === undefined || finerThings === undefined || experience === undefined || car ===undefined || obstacles === undefined) { 
       alert('Please fill out form completely');
-    } else if (
+    } else if (notice === "yes" && finerThings === "jewelery" && experience === "no" && car === "safe" && obstacles === "frustrating") {
+      $("#pythonResult").show();
+      $("#javaScriptResult").hide();
+      $("#rubyResult").hide();
+      $("#rustResult").hide();    
+    } else if (notice === "no" && finerThings === "snakeSkin" && experience === "yes" && car === "muscle" && obstacles === "learning") {
+      $("#pythonResult").hide();
+      $("#javaScriptResult").show();
+      $("#rubyResult").hide();
+      $("#rustResult").hide(); 
+    } else if (notice === "yes" && finerThings === "snakeSkin" && experience === "yes" && car === "safe" && obstacles === "frustrating") {
+      $("#pythonResult").hide();
+      $("#javaScriptResult").hide();
+      $("#rubyResult").show();
+      $("#rustResult").hide(); 
+    } else {
+      $("#pythonResult").hide();
+      $("#javaScriptResult").hide();
+      $("#rubyResult").hide();
+      $("#rustResult").show();
+
+    }
+
 
     $(".name1").text(name1Input);
     $(".surveyDate").text(surveyDateInput);
